911fb81e1658188999d763cdc8fdc1dc6e4ab240
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-15 17:10:42 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -9,6 +9,9 @@ class Customer
    def self.all
      @@all
    end
+   def new_meal(waiter, total, tip=0)
+     Meal.new(waiter, self, total, tip)
+   end
    def meals
      Meal.all.select do |meal|
        meal.customer == self
@@ -18,9 +21,6 @@ class Customer
      meals.map do |meal|
        meal.waiter
      end
-   end
-   def new_meal(waiter, total, tip=0)
-     Meal.new(waiter, self, total, tip)
    end
   def new_meal_20_percent(waiter, total)
     tip = total * 0.2
